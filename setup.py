170ac2a47ad388eb9629b1758c4618c4972f002b
Marina Kiseleva <marinaki@vt.edu>
2020-06-10 13:16:31 -0700

    update description and name

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 from distutils.core import setup
 
-setup(name='dist_code',
+setup(name='z_evaluator',
       packages=find_packages(),
       version='2.4',
-      description='Redshift Dist Matching',
+      description='Tools to compare and evaluate the redshift distributions of transient clases in the PLASTICCS and THEx datasets.',
       author='Marina Kisley',
       author_email='marinaki@email.arizona.edu'
       )
